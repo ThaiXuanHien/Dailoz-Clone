ad083692a9cf0c542a20859b77cd6ce7a79c487c
hien.thai <hien.thai@ntq-solution.com.vn>
2023-11-29 18:25:28 +0700

    # splash screen

--- app/src/main/java/com/hienthai/dailoz_clone/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/hienthai/dailoz_clone/screens/splash/SplashScreen.kt
@@ -1,16 +1,15 @@
 package com.hienthai.dailoz_clone.screens.splash
 
-import android.text.Spannable
-import android.window.SplashScreen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -38,53 +37,65 @@ fun SplashScreen() {
     Surface(color = Color.White) {
         Box(
             modifier = Modifier.fillMaxSize()
-            ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Spacer(modifier = Modifier.height(128.dp))
-                Image(
-                    modifier = Modifier.padding(horizontal = 42.dp),
-                    painter = painterResource(id = R.drawable.img_logo),
-                    contentDescription = ""
-                )
-                Spacer(modifier = Modifier.height(75.dp))
-                TextAppName()
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(
-                    modifier = Modifier.padding(horizontal = 26.dp),
-                    fontSize = 14.sp,
-                    color = colorResource(id = R.color.color_text_content_splash),
-                    textAlign = TextAlign.Center,
-                    text = "Plan what you will do to be more organized for today, \ntomorrow and beyond"
-                )
-                Spacer(modifier = Modifier.height(65.dp))
-                Button(
-                    modifier = Modifier
-                        .height(52.dp)
-                        .fillMaxWidth()
-                        .padding(horizontal = 35.dp),
-                    shape = RoundedCornerShape(14.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        colorResource(R.color.base_color)
-                    ),
-                    onClick = { /*TODO*/ }) {
-                    Text(
-                        text = "Login",
-                        fontSize = 16.sp,
-                        color = Color.White,
-                        fontWeight = FontWeight.Bold
+        ) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
+                    Image(
+                        modifier = Modifier.padding(horizontal = 42.dp),
+                        painter = painterResource(id = R.drawable.img_logo),
+                        contentDescription = ""
                     )
                 }
-                Spacer(modifier = Modifier.height(24.dp))
-                Text(
-                    fontSize = 16.sp,
-                    color = colorResource(R.color.base_color),
-                    textAlign = TextAlign.Center,
-                    fontWeight = FontWeight.Bold,
-                    text = "Sign Up"
-                )
+
+                Box(
+                    modifier = Modifier.height(IntrinsicSize.Min),
+                    contentAlignment = Alignment.BottomCenter
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(bottom = 55.dp)
+                            .wrapContentSize(align = Alignment.BottomCenter),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+
+                        TextAppName()
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Text(
+                            modifier = Modifier.padding(horizontal = 26.dp),
+                            fontSize = 14.sp,
+                            color = colorResource(id = R.color.color_text_content_splash),
+                            textAlign = TextAlign.Center,
+                            text = "Plan what you will do to be more organized for today, \ntomorrow and beyond"
+                        )
+                        Spacer(modifier = Modifier.height(65.dp))
+                        Button(
+                            modifier = Modifier
+                                .height(52.dp)
+                                .fillMaxWidth()
+                                .padding(horizontal = 35.dp),
+                            shape = RoundedCornerShape(14.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                colorResource(R.color.base_color)
+                            ),
+                            onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "Login",
+                                fontSize = 16.sp,
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(24.dp))
+                        Text(
+                            fontSize = 16.sp,
+                            color = colorResource(R.color.base_color),
+                            textAlign = TextAlign.Center,
+                            fontWeight = FontWeight.Bold,
+                            text = "Sign Up"
+                        )
+                    }
+                }
             }
         }
     }
